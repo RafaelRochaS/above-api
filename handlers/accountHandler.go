b7e77b6798cb2ba43cf9ce5e6641102ca2897788
rafael <rafaelrochasouza@outlook.com.br>
2023-04-21 20:20:16 -0300

    add: call internal grpc service for account creation

--- handlers/accountHandler.go
+++ handlers/accountHandler.go
@@ -1,14 +1,21 @@
 package handlers
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/RafaelRochaS/above-api/models"
+	"github.com/RafaelRochaS/above-api/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/lithammer/shortuuid"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/RafaelRochaS/above-api/above_api"
 )
 
 func HandleAccounts(rg *gin.RouterGroup) {
@@ -21,6 +28,7 @@ func handleAccountCreation(ctx *gin.Context) {
 	var user models.User
 
 	if err := ctx.ShouldBindJSON(&user); err != nil {
+		log.Printf("AccountHandler :: Error binding input: %v", err)
 		ctx.JSON(http.StatusBadRequest, err.Error())
 		return
 	}
@@ -32,6 +40,12 @@ func handleAccountCreation(ctx *gin.Context) {
 	}
 
 	printUser(*userDto)
+	err := callgRPCAccountCreation(*userDto)
+	if err != nil {
+		log.Printf("AccountHandler :: Error calling gRPC service: %v", err)
+		ctx.JSON(http.StatusInternalServerError, fmt.Sprintf("Internal Server Error\n%v", err.Error()))
+		return
+	}
 
 	ctx.JSON(http.StatusCreated, "[WIP] account created")
 }
@@ -44,4 +58,43 @@ func printUser(user models.UserDto) {
 	log.Println("Age: ", user.User.Age)
 	log.Println("Time: ", user.Timestamp)
 	log.Println("TrxID: ", user.TrxId)
+}
+
+func callgRPCAccountCreation(user models.UserDto) error {
+	conn, err := grpc.Dial(
+		fmt.Sprintf("%s:%d", utils.ACCOUNT_SERVICE_GRPC_HOST, utils.ACCOUNT_SERVICE_GRPC_PORT),
+		grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Printf("AccountHandler :: Error creating gRPC connection: %v", err)
+		return err
+	}
+
+	defer conn.Close()
+
+	client := pb.NewUserClient(conn)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+
+	response, err := client.CreateUser(ctx,
+		&pb.UserCreateRequest{
+			FirstName: user.User.FirstName,
+			LastName:  user.User.LastName,
+			Email:     user.User.Email,
+			Address:   user.User.Address,
+			Age:       int32(user.User.Age),
+		})
+
+	if err != nil {
+		log.Printf("AccountHandler :: Error calling gRPC service: %v", err)
+		return err
+	}
+
+	log.Printf("AccountHandler :: Server response: %v", response.GetStatus())
+
+	if response.GetStatus() != "success" {
+		return errors.New("account creation service returned failure")
+	}
+
+	return nil
 }
--- utils/config.go
+++ utils/config.go
@@ -11,6 +11,8 @@ import (
 var HOST string
 var PORT int
 var TRUSTED_PROXIES string
+var ACCOUNT_SERVICE_GRPC_HOST string
+var ACCOUNT_SERVICE_GRPC_PORT int
 
 func LoadEnvs() {
 	err := godotenv.Load()
@@ -18,11 +20,20 @@ func LoadEnvs() {
 		log.Fatal("Error loading .env file")
 	}
 
+	var errPort error
+	var errAccSerPort error
+
 	HOST = os.Getenv("HOST")
-	PORT, err = strconv.Atoi(os.Getenv("PORT"))
+	PORT, errPort = strconv.Atoi(os.Getenv("PORT"))
+	ACCOUNT_SERVICE_GRPC_HOST = os.Getenv("ACCOUNT_SERVICE_GRPC_HOST")
+	ACCOUNT_SERVICE_GRPC_PORT, errAccSerPort = strconv.Atoi(os.Getenv("ACCOUNT_SERVICE_GRPC_PORT"))
 	TRUSTED_PROXIES = os.Getenv("TRUSTED_PROXIES")
 
-	if err != nil {
+	if errPort != nil {
 		PORT = 3000
+	}
+
+	if errAccSerPort != nil {
+		PORT = 50000
 	}
 }
